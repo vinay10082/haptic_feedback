f75f475e288afc092389dd82192fb6a963fb176d
vinay10082 <20bph043@nith.ac.in>
2024-04-20 23:16:54 +0530

    implement indicators

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:flutter_tts/flutter_tts.dart';
 
+import 'package:flutter_tts/flutter_tts.dart';
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -11,6 +14,7 @@ class CameraScreen extends StatefulWidget {
 class _CameraScreenState extends State<CameraScreen> {
   late CameraController _controller;
   late Future<void> _initializeControllerFuture;
+
   late FlutterTts flutterTts; // Add FlutterTts instance
 
   playVoice(String s) async {
@@ -23,32 +27,59 @@ class _CameraScreenState extends State<CameraScreen> {
     await flutterTts.speak(s);
   }
 
+  // Add variables to track obstacle positions
+  bool obstacleLeft = false;
+  bool obstacleRight = false;
+
+// Function to start obstacle detection
+  void startObstacleDetection() {
+    //For Example
+    // Initialize a random number generator
+    var random = Random();
+    // Simulate obstacle detection with random values
+    Timer.periodic(const Duration(seconds: 5), (timer) {
+      setState(() {
+        // Simulate obstacle detection on the left
+        obstacleLeft = random.nextBool();
+        // Simulate obstacle detection on the right
+        obstacleRight = random.nextBool();
+        if (obstacleLeft == true && obstacleRight == false) {
+          playVoice('There is Obstacle in yours left, please go right');
+        } 
+        else if (obstacleRight == true && obstacleLeft == false) {
+          playVoice('There is Obstacle in yours right, please go left');
+        } 
+        else if (obstacleLeft == true && obstacleRight == true) {
+          playVoice('Please stop, obstacle ahead');
+        }
+      });
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    // Get the list of available cameras
     availableCameras().then((cameras) {
-      // Get the first available camera
       final firstCamera = cameras.first;
-      // Initialize the camera controller
       _controller = CameraController(
         firstCamera,
         ResolutionPreset.medium,
       );
-      // Initialize the camera controller future
       _initializeControllerFuture = _controller.initialize();
+      _initializeControllerFuture.then((_) {
+        // Start obstacle detection once camera is initialized
+        startObstacleDetection();
+      });
 
       flutterTts = FlutterTts(); // Initialize FlutterTts
-      // Set state after initializing the controller
       setState(() {});
     });
   }
 
   @override
   void dispose() {
-        // Dispose of the FlutterTts instance when not needed
+    // Dispose of the FlutterTts instance when not needed
     flutterTts.stop(); // Stop speaking
-    // Dispose of the camera controller when not needed
     _controller.dispose();
     super.dispose();
   }
@@ -71,14 +102,11 @@ class _CameraScreenState extends State<CameraScreen> {
                 child: Text('Error: ${snapshot.error}'),
               );
             } else {
-              // Get the screen size
               final size = MediaQuery.of(context).size;
-
               return Stack(
                 children: [
                   Container(
                     color: Colors.black,
-                    // Ensure that the camera preview fills the entire screen
                     child: Center(
                       child: AspectRatio(
                         aspectRatio: _controller.value.aspectRatio,
@@ -88,7 +116,6 @@ class _CameraScreenState extends State<CameraScreen> {
                             child: FittedBox(
                               fit: BoxFit.cover,
                               child: SizedBox(
-                                // Set width and height to screen dimensions
                                 width: size.width,
                                 height: size.height,
                                 child: CameraPreview(_controller),
@@ -99,59 +126,90 @@ class _CameraScreenState extends State<CameraScreen> {
                       ),
                     ),
                   ),
-                  //Buttons
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    bottom: 20.0, // Adjust this value for desired bottom margin
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            playVoice('There is Obstacle in yours right, please go left');
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.red),
-                          child: Text(
-                            'LEFT',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
+                  // Add obstacle indicators
+                  if (obstacleLeft == true && obstacleRight == false)
+                    Positioned(
+                      left: 0,
+                      top: size.height / 2 -
+                          (size.height / 2.8) / 2, // Center vertically
+                      child: Container(
+                          height: size.height / 2.8,
+                          width: size.width / 2,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.red, // Border color
+                              width: 2.0, // Border width
                             ),
+                            color: Colors.transparent, // Transparent background
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            playVoice('Please stop, obstacle ahead');
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.green),
-                          child: Text(
-                            'AHEAD',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            playVoice('There is Obstacle in yours left, please go right');
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blue),
-                          child: Text(
-                            'RIGHT',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                'LEFT',
+                                style: TextStyle(
+                                  color: Colors.red, // White text
+                                  fontSize: 20.0, // Text size
+                                ),
+                              ),
+                            ],
+                          )),
                     ),
-                  ),
+                  if (obstacleRight == true && obstacleLeft == false)
+                    Positioned(
+                      right: 0,
+                      top: size.height / 2 -
+                          (size.height / 2.8) / 2, // Center vertically
+                      child: Container(
+                          height: size.height / 2.8,
+                          width: size.width / 2,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.red, // Border color
+                              width: 2.0, // Border width
+                            ),
+                            color: Colors.transparent, // Transparent background
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                'RIGHT',
+                                style: TextStyle(
+                                  color: Colors.red, // White text
+                                  fontSize: 20.0, // Text size
+                                ),
+                              ),
+                            ],
+                          )),
+                    ),
+                  if (obstacleRight == true && obstacleLeft == true)
+                    Positioned(
+                      top: size.height / 2 -
+                          (size.height / 2.8) / 2, // Center vertically
+                      child: Container(
+                          height: size.height / 2.8,
+                          width: size.width,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.red, // Border color
+                              width: 2.0, // Border width
+                            ),
+                            color: Colors.transparent, // Transparent background
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'AHEAD',
+                                style: TextStyle(
+                                  color: Colors.red, // White text
+                                  fontSize: 20.0, // Text size
+                                ),
+                              ),
+                            ],
+                          )),
+                    ),
                 ],
               );
             }
